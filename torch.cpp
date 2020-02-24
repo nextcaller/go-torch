0d351277b18717921df70b5794ae6ac7efe88285
Cesar Del Solar <cesar@nextcaller.com>
2020-02-24 16:43:50 -0500

    comment out some potentially unneeded methods

--- torch.cpp
+++ torch.cpp
@@ -209,19 +209,19 @@ void Torch_DeleteTensor(Torch_TensorContext ctx) {
 
 }
 
-Torch_JITModuleContext Torch_CompileTorchScript(char* cstring_script, Torch_Error* error) {
-    HANDLE_TH_ERRORS
-    std::string script(cstring_script);
-    //std::istringstream is(cstring_script);
-    auto mod = new Torch_JITModule();
-    c10::QualifiedName base("__script__");
-    //std::shared_ptr<torch::jit::script::Module> module(base, torch::jit::compile(script));
+// Torch_JITModuleContext Torch_CompileTorchScript(char* cstring_script, Torch_Error* error) {
+//     HANDLE_TH_ERRORS
+//     std::string script(cstring_script);
+//     //std::istringstream is(cstring_script);
+//     auto mod = new Torch_JITModule();
+//     c10::QualifiedName base("__script__");
+//     //std::shared_ptr<torch::jit::script::Module> module(base, torch::jit::compile(script));
 
-    mod->module = std::make_shared<torch::jit::script::Module>(torch::jit::script::Module(base, torch::jit::compile(script)));
+//     mod->module = std::make_shared<torch::jit::script::Module>(torch::jit::script::Module(base, torch::jit::compile(script)));
 
-    return (void *)mod;
-    END_HANDLE_TH_ERRORS(error, NULL)
-}
+//     return (void *)mod;
+//     END_HANDLE_TH_ERRORS(error, NULL)
+// }
 
 Torch_JITModuleContext Torch_LoadJITModule(char* cstring_path, Torch_Error* error) {
     HANDLE_TH_ERRORS
@@ -240,13 +240,13 @@ Torch_JITModuleContext Torch_LoadJITModule(char* cstring_path, Torch_Error* erro
     END_HANDLE_TH_ERRORS(error, NULL)
 }
 
-void Torch_ExportJITModule(Torch_JITModuleContext ctx, char* cstring_path, Torch_Error* error) {
-    HANDLE_TH_ERRORS
-    std::string module_path(cstring_path);
-    auto mod = (Torch_JITModule*)ctx;
-    mod->module->save(module_path);
-    END_HANDLE_TH_ERRORS(error,)
-}
+// void Torch_ExportJITModule(Torch_JITModuleContext ctx, char* cstring_path, Torch_Error* error) {
+//     HANDLE_TH_ERRORS
+//     std::string module_path(cstring_path);
+//     auto mod = (Torch_JITModule*)ctx;
+//     mod->module->save(module_path);
+//     END_HANDLE_TH_ERRORS(error,)
+// }
 
 Torch_JITModuleMethodContext Torch_JITModuleGetMethod(Torch_JITModuleContext ctx, char* cstring_method, Torch_Error* error) {
     HANDLE_TH_ERRORS
@@ -262,25 +262,25 @@ Torch_JITModuleMethodContext Torch_JITModuleGetMethod(Torch_JITModuleContext ctx
 }
 
 
-char** Torch_JITModuleGetMethodNames(Torch_JITModuleContext ctx, size_t* len) {
-    auto mod = (Torch_JITModule*)ctx;
-    auto size = mod->module->get_methods().size();
-    *len = size;
-    auto result = (char**)malloc(sizeof(char*) * size);
+// char** Torch_JITModuleGetMethodNames(Torch_JITModuleContext ctx, size_t* len) {
+//     auto mod = (Torch_JITModule*)ctx;
+//     auto size = mod->module->get_methods().size();
+//     *len = size;
+//     auto result = (char**)malloc(sizeof(char*) * size);
 
-    int i = 0;
-    for (auto& method : mod->module->get_methods()) {
-        auto key = method.name();
-        auto ckey = new char[key.length() + 1];
-        strcpy(ckey, key.c_str());
+//     int i = 0;
+//     for (auto& method : mod->module->get_methods()) {
+//         auto key = method.name();
+//         auto ckey = new char[key.length() + 1];
+//         strcpy(ckey, key.c_str());
 
-        *(result + i) = ckey;
+//         *(result + i) = ckey;
 
-        i++;
-    }
+//         i++;
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
 Torch_IValue Torch_JITModuleMethodRun(Torch_JITModuleMethodContext ctx, Torch_IValue* inputs, size_t input_size, Torch_Error* error) {
     HANDLE_TH_ERRORS
@@ -299,59 +299,59 @@ Torch_IValue Torch_JITModuleMethodRun(Torch_JITModuleMethodContext ctx, Torch_IV
 }
 
 
-Torch_ModuleMethodArgument* Torch_JITModuleMethodArguments(Torch_JITModuleMethodContext ctx, size_t* res_size) {
-    auto met = (Torch_JITModule_Method*)ctx;
-    auto schema = met->run.function().getSchema();
-    auto arguments = schema.arguments();
+// Torch_ModuleMethodArgument* Torch_JITModuleMethodArguments(Torch_JITModuleMethodContext ctx, size_t* res_size) {
+//     auto met = (Torch_JITModule_Method*)ctx;
+//     auto schema = met->run.function().getSchema();
+//     auto arguments = schema.arguments();
 
-    auto result = (Torch_ModuleMethodArgument*)malloc(sizeof(Torch_ModuleMethodArgument)*arguments.size());
-    *res_size = arguments.size();
+//     auto result = (Torch_ModuleMethodArgument*)malloc(sizeof(Torch_ModuleMethodArgument)*arguments.size());
+//     *res_size = arguments.size();
 
-    for(std::vector<torch::Argument>::size_type i = 0; i != arguments.size(); i++) {
-        auto name = arguments[i].name();
-        char *cstr_name = new char[name.length() + 1];
-        strcpy(cstr_name, name.c_str());
+//     for(std::vector<torch::Argument>::size_type i = 0; i != arguments.size(); i++) {
+//         auto name = arguments[i].name();
+//         char *cstr_name = new char[name.length() + 1];
+//         strcpy(cstr_name, name.c_str());
 
-        auto type = arguments[i].type()->str();
-        char *cstr_type = new char[type.length() + 1];
-        strcpy(cstr_type, type.c_str());
+//         auto type = arguments[i].type()->str();
+//         char *cstr_type = new char[type.length() + 1];
+//         strcpy(cstr_type, type.c_str());
 
-        *(result + i) = Torch_ModuleMethodArgument{
-            .name = cstr_name,
-            .typ = cstr_type,
-        };
-    }
+//         *(result + i) = Torch_ModuleMethodArgument{
+//             .name = cstr_name,
+//             .typ = cstr_type,
+//         };
+//     }
 
-    return result;
-}
-
-
-Torch_ModuleMethodArgument* Torch_JITModuleMethodReturns(Torch_JITModuleMethodContext ctx, size_t* res_size) {
-    auto met = (Torch_JITModule_Method*)ctx;
-    auto schema = met->run.function().getSchema();
-    auto arguments = schema.returns();
-
-    auto result = (Torch_ModuleMethodArgument*)malloc(sizeof(Torch_ModuleMethodArgument)*arguments.size());
-    *res_size = arguments.size();
-
-    for(std::vector<torch::Argument>::size_type i = 0; i != arguments.size(); i++) {
-        auto name = arguments[i].name();
-        char *cstr_name = new char[name.length() + 1];
-        strcpy(cstr_name, name.c_str());
-
-        auto type = arguments[i].type()->str();
-        char *cstr_type = new char[type.length() + 1];
-        strcpy(cstr_type, type.c_str());
+//     return result;
+// }
 
 
-        *(result + i) = Torch_ModuleMethodArgument{
-            .name = cstr_name,
-            .typ = cstr_type,
-        };
-    }
+// Torch_ModuleMethodArgument* Torch_JITModuleMethodReturns(Torch_JITModuleMethodContext ctx, size_t* res_size) {
+//     auto met = (Torch_JITModule_Method*)ctx;
+//     auto schema = met->run.function().getSchema();
+//     auto arguments = schema.returns();
 
-    return result;
-}
+//     auto result = (Torch_ModuleMethodArgument*)malloc(sizeof(Torch_ModuleMethodArgument)*arguments.size());
+//     *res_size = arguments.size();
+
+//     for(std::vector<torch::Argument>::size_type i = 0; i != arguments.size(); i++) {
+//         auto name = arguments[i].name();
+//         char *cstr_name = new char[name.length() + 1];
+//         strcpy(cstr_name, name.c_str());
+
+//         auto type = arguments[i].type()->str();
+//         char *cstr_type = new char[type.length() + 1];
+//         strcpy(cstr_type, type.c_str());
+
+
+//         *(result + i) = Torch_ModuleMethodArgument{
+//             .name = cstr_name,
+//             .typ = cstr_type,
+//         };
+//     }
+
+//     return result;
+// }
 
 
 void Torch_DeleteJITModuleMethod(Torch_JITModuleMethodContext ctx) {
